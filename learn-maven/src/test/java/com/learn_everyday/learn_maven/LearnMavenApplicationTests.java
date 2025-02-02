77ecc1b331111d7ad496f829bd4fda2cb38758ef
JagatheshwaranN <jagatheshwaran.n29@gmail.com>
2025-02-02 16:12:36 +0530

    Third Commit

---
+++ learn-maven/src/test/java/com/learn_everyday/learn_maven/LearnMavenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn_everyday.learn_maven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnMavenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
