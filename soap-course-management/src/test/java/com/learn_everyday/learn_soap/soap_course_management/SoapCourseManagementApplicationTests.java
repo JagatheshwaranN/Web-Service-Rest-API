ea3d3d7484c7b94065493ef897e13d48df00c1cd
JagatheshwaranN <jagatheshwaran.n29@gmail.com>
2025-02-03 21:03:09 +0530

    SOAP Web Service - First Commit

---
+++ soap-course-management/src/test/java/com/learn_everyday/learn_soap/soap_course_management/SoapCourseManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn_everyday.learn_soap.soap_course_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoapCourseManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
