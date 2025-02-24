928ca0128bdb8d4ec5d48918d9a30e227fd81e17
JagatheshwaranN <jagatheshwaran.n29@gmail.com>
2025-02-24 08:58:12 +0530

    Use of Context in XML Config, ComponentScan

--- spring-basics/src/main/java/com/learn_everyday/spring_basics/SpingXmlConfigApplication.java
+++ spring-basics/src/main/java/com/learn_everyday/spring_basics/SpingXmlConfigApplication.java
@@ -2,12 +2,10 @@ package com.learn_everyday.spring_basics;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import com.learn_everyday.spring_basics.xml.XmlUserDAO;
 
-@Configuration
 public class SpingXmlConfigApplication {
 
 	private static Logger LOGGER = LoggerFactory.getLogger(SpingXmlConfigApplication.class);
@@ -15,6 +13,7 @@ public class SpingXmlConfigApplication {
 	public static void main(String[] args) {
 
 		try (ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml")) {
+			LOGGER.info("Beans Loaded -> {}", (Object)context.getBeanDefinitionNames());
 			XmlUserDAO userDAO1 = context.getBean(XmlUserDAO.class);
 			LOGGER.info("{}", userDAO1);
 			LOGGER.info("{}", userDAO1.getXmlJdbcConnection());
