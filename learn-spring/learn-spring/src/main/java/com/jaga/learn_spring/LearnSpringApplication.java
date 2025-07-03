3931eca458c0fd863c81b78a3ad0a1f387266e27
JagatheshwaranN <jagatheshwaran.n29@gmail.com>
2025-07-02 21:12:19 -0500

    Spring Revise

---
+++ learn-spring/learn-spring/src/main/java/com/jaga/learn_spring/LearnSpringApplication.java
@@ -0,0 +1,15 @@
+package com.jaga.learn_spring;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class LearnSpringApplication {
+
+	public static void main(String[] args) {
+		ApplicationContext context = new ClassPathXmlApplicationContext("spring-config.xml");
+		// Student student = context.getBean(Student.class);
+		Student student = (Student) context.getBean("student2");
+		student.show();
+	}
+
+}
---
+++ learn-spring/learn-spring/src/main/java/com/jaga/learn_spring/Student.java
@@ -0,0 +1,12 @@
+package com.jaga.learn_spring;
+
+public class Student {
+
+	public Student() {
+		System.out.println("Student is created..");
+	}
+
+	public void show() {
+		System.out.println("Display Student");
+	}
+}
